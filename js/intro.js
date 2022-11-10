db8a2d85bd37d626444e4782bfd7111d48958479
Subin Yook <sprout.jessie@gmail.com>
2022-11-11 00:28:41 +0900

    implement a intro page

--- js/intro.js
+++ js/intro.js
@@ -1,11 +1,11 @@
 // show intro-setting
 const addButton = document.querySelector('.intro-add')
-const introFirst = document.querySelector('.intro-guide')
-const introSecond = document.querySelector('.intro-setting')
+const introGuide = document.querySelectorAll('.intro-guide')
+const introSettding = document.querySelector('.intro-setting')
 
 function showIntroSetting() {
-  introFirst.classList.toggle('visually-hidden')
-  introSecond.classList.toggle('visually-hidden')
+  introGuide.forEach((i) => i.classList.toggle('visually-hidden'))
+  introSettding.classList.toggle('visually-hidden')
 }
 
 addButton.addEventListener('click', showIntroSetting)
@@ -20,7 +20,14 @@ for (let i = 1; i <= 30; i++) {
   introSelect.appendChild(option)
 }
 
-// from number to currency in bedget input
+// change color of select
+function changeColor() {
+  introSelect.style.color = '#3d434b'
+}
+
+introSelect.addEventListener('change', changeColor)
+
+// change to currency in bedget input
 const budgetInput = document.querySelector('.intro-setting .input')
 
 function toCurrency(e) {
@@ -31,3 +38,15 @@ function toCurrency(e) {
 }
 
 budgetInput.addEventListener('keyup', toCurrency)
+
+// set budget data
+const startButton = document.querySelector('.intro-start-button')
+const intro = document.querySelector('.intro')
+const spend = document.querySelector('.spend')
+
+function showSpend() {
+  intro.classList.toggle('visually-hidden')
+  spend.classList.toggle('visually-hidden')
+}
+
+startButton.addEventListener('click', showSpend)
