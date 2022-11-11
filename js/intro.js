03fbe23450c156de1e8c61fc780afbea8447ea4b
Subin Yook <sprout.jessie@gmail.com>
2022-11-11 19:51:02 +0900

    define functions for a startButton

--- js/intro.js
+++ js/intro.js
@@ -1,52 +1,68 @@
-// show intro-setting
-const addButton = document.querySelector('.intro-add')
+const introAddButon = document.querySelector('.intro-add')
 const introGuide = document.querySelectorAll('.intro-guide')
 const introSettding = document.querySelector('.intro-setting')
 
+const startDaySelect = document.querySelector('.intro-setting .select')
+// declared at globals.js
+// const budgetInput = document.querySelector('.money-input input')
+
+const startButton = document.querySelector('.intro-start-button')
+const introPage = document.querySelector('.intro')
+const spendPage = document.querySelector('.spend')
+
+// show intro-setting
 function showIntroSetting() {
   introGuide.forEach((i) => i.classList.toggle('visually-hidden'))
   introSettding.classList.toggle('visually-hidden')
 }
 
-addButton.addEventListener('click', showIntroSetting)
+introAddButon.addEventListener('click', showIntroSetting)
 
 // add start-day options
-const introSelect = document.querySelector('.intro-setting .select')
-
 for (let i = 1; i <= 30; i++) {
   const option = document.createElement('option')
   option.value = `${i}`
   option.text = `${i}일`
-  introSelect.appendChild(option)
+  startDaySelect.appendChild(option)
 }
 
 // change color of select
 function changeColor() {
-  introSelect.style.color = '#3d434b'
+  startDaySelect.style.color = '#3d434b'
 }
 
-introSelect.addEventListener('change', changeColor)
+startDaySelect.addEventListener('change', changeColor)
 
-// change to currency in bedget input
-const budgetInput = document.querySelector('.intro-setting .input')
+// activate the start button
+function activeStartButton() {
+  if (startDaySelect.value !== '' && budgetInput.value !== '') {
+    const alertText = document.querySelector('.intro-alert-text')
 
-function toCurrency(e) {
-  let value = e.target.value
-  value = Number(value.replaceAll(',', ''))
-  let formatValue = value.toLocaleString('ko-KR')
-  budgetInput.value = formatValue
+    if (budgetInput.value < 1000) {
+      alertText.classList.remove('visually-hidden')
+      alertText.innerText = '1,000원 이상의 예산을 설정해주세요'
+      startButton.setAttribute('disabled', '')
+      return
+    }
+    alertText.classList.add('visually-hidden')
+    startButton.removeAttribute('disabled')
+  }
 }
-
-budgetInput.addEventListener('keyup', toCurrency)
+startDaySelect.addEventListener('change', activeStartButton)
+budgetInput.addEventListener('keyup', activeStartButton)
 
 // set budget data
-const startButton = document.querySelector('.intro-start-button')
-const intro = document.querySelector('.intro')
-const spend = document.querySelector('.spend')
+// value값을 로컬에 저장
+// 페이지 전환
 
 function showSpend() {
-  intro.classList.toggle('visually-hidden')
-  spend.classList.toggle('visually-hidden')
+  introPage.classList.toggle('visually-hidden')
+  spendPage.classList.toggle('visually-hidden')
+
+  console.log(startDaySelect.value)
+  console.log(budgetInput.value)
 }
 
 startButton.addEventListener('click', showSpend)
+
+// set user-data in local
