e46d6c2f188f2bf9a55c42b1ccf0c062a9d1a6a4
Subin Yook <sprout.jessie@gmail.com>
2022-11-11 17:17:15 +0900

    define a toCurrency function at globals.js

---
+++ js/globals.js
@@ -0,0 +1,11 @@
+// change to currency in bedget input
+const budgetInput = document.querySelector('.money-input input')
+
+function toCurrency(e) {
+  let value = e.target.value
+  value = Number(value.replaceAll(',', ''))
+  let formatValue = value.toLocaleString('ko-KR')
+  budgetInput.value = formatValue
+}
+
+budgetInput.addEventListener('keyup', toCurrency)
